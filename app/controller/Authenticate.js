452bbadb6c76760a5773711aec74584f2267e118
Nikhilm <shikarkhane@gmail.com>
2015-01-05 13:58:00 +0100

    ajax call - sms verify

--- app/controller/Authenticate.js
+++ app/controller/Authenticate.js
@@ -14,10 +14,46 @@ Ext.define('ttapp.controller.Authenticate', {
     },
     sendConfirmationCode: function(){
         console.log('send confirmation code!');
+
+        Ext.Ajax.request({
+                            url: 'http://localhost:8888/sms-code/',
+                            method: 'POST',
+                            headers: { 'Content-Type': 'application/json'},
+                            disableCaching: false,
+                            jsonData: {
+                                "to_user": "nikhil"
+                            },
+
+                            success: function(response) {
+                                console.log(response.responseText);
+                            }
+                        });
         Ext.Viewport.setActiveItem('confirmphonenumber');
     },
     confirmCode: function(){
         console.log('confirm code');
-        Ext.Viewport.setActiveItem('tink');
+        Ext.Ajax.request({
+                            url: 'http://localhost:8888/verify-user/',
+                            method: 'POST',
+                            headers: { 'Content-Type': 'application/json'},
+                            disableCaching: false,
+                            jsonData: {
+                                "to_user": "nikhil",
+                                "code" : "1234"
+                            },
+
+                            success: function(response) {
+                                //debugger;
+                                console.log(response.responseText);
+                                debugger;
+                                if ( JSON.parse(response.responseText)['status'] == true){
+                                    Ext.Viewport.setActiveItem('tink');                             
+                                }
+                                else{
+                                    console.log('Verification code doesnt match');
+                                }
+                            }
+                        });
+        
     }
 });
