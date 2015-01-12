15bab9f5a7889c14397be8fe1bcd02076f652c10
Nikhil <nikhil.shikarkhane@comeon.com>
2015-01-12 14:54:40 +0100

    chosen trinket is set in iframe

--- app/controller/tink.js
+++ app/controller/tink.js
@@ -10,30 +10,37 @@ Ext.define('ttapp.controller.Tink', {
         control: {
             'tink': {
                 startedthinking: 'onThinking',
-                stoppedthinking: 'onStoppedThinking'
+                stoppedthinking: 'onStoppedThinking',
+                show: 'useActiveTrinket'
             },
             'tink panel':{
                 choosetrinket: 'onChooseTrinket'
             }
         }
     },
-onChooseTrinket: function(){
-    Ext.Viewport.setActiveItem('trinket');
-},
-onThinking : function(){
-    //console.log('thinking');
-    this.getClock().start();
-    Ext.getDom('tinkcontainer').contentWindow.tt_start_animation();
-    
-},
-onStoppedThinking : function(){
-    //console.log('stopped thinking');
+    onChooseTrinket: function(){
+        Ext.Viewport.setActiveItem('trinket');
+    },
+    onThinking : function(){
+        //console.log('thinking');
 
-    Ext.getDom('tinkcontainer').contentWindow.tt_stop_animation();
-    this.getClock().pause();
-    var periodInSeconds = this.getClock().getDuration();
+        this.getClock().start();
+        Ext.getDom('tinkcontainer').contentWindow.tt_start_animation();
 
-    this.getApplication().getController('SendTo').showSendTo(periodInSeconds, 1);
-}
+    },
+    onStoppedThinking : function(){
+        //console.log('stopped thinking');
+
+        Ext.getDom('tinkcontainer').contentWindow.tt_stop_animation();
+        this.getClock().pause();
+        var periodInSeconds = this.getClock().getDuration();
+
+        this.getApplication().getController('SendTo').showSendTo(periodInSeconds, 1);
+    },
+    useActiveTrinket : function(){
+        var trinketArea = Ext.get('swiffydiv');
+        var activeTrinket = Ext.getStore('profilestore').getActiveTrinket();
+        trinketArea.setHtml('<iframe id="tinkcontainer" style="width:350px;height:500px;" src="resources/tinks/'+ activeTrinket + '"></iframe>');
+    }
 
 });
