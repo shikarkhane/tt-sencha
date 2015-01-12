079c6e8bb2bee060ffffb4f5c85c6ce321b07587
Nikhil <nikhil.shikarkhane@comeon.com>
2015-01-12 16:34:35 +0100

    search on receipients

--- app/controller/SendTo.js
+++ app/controller/SendTo.js
@@ -9,15 +9,65 @@ Ext.define('ttapp.controller.SendTo', {
         control: {
             'searchContactsField': {
                 // todo not working yet
-                keyup: 'searchReady'
+                keyup: 'onSearchKeyUp',
+                clearicontap: 'onSearchClearIconTap'
             },
             'sendto list': {
                 itemtap: 'composeTink'
             }
         }
     },
-    searchReady : function(){
-        console.log('ready to search');
+    onSearchClearIconTap: function() {
+        //call the clearFilter method on the store instance
+        Ext.getStore('Contacts').getStore().clearFilter();
+    },
+    onSearchKeyUp : function(field){
+        var value = field.getValue(),
+            store = Ext.getStore('Contacts');
+
+        //first clear any current filters on thes tore
+        store.clearFilter();
+
+        //check if a value is set first, as if it isnt we dont have to do anything
+        if (value) {
+            //the user could have entered spaces, so we must split them so we can loop through them all
+            var searches = value.split(' '),
+                regexps = [],
+                i;
+
+            //loop them all
+            for (i = 0; i < searches.length; i++) {
+                //if it is nothing, continue
+                if (!searches[i]) continue;
+
+                //if found, create a new regular expression which is case insenstive
+                regexps.push(new RegExp(searches[i], 'i'));
+            }
+
+            //now filter the store by passing a method
+            //the passed method will be called for each record in the store
+            store.filter(function(record) {
+                var matched = [];
+
+                //loop through each of the regular expressions
+                for (i = 0; i < regexps.length; i++) {
+                    var search = regexps[i],
+                        didMatch = record.get('first_name').match(search) || record.get('last_name').match(search);
+
+                    //if it matched the first or last name, push it into the matches array
+                    matched.push(didMatch);
+                }
+
+                //if nothing was found, return false (dont so in the store)
+                if (regexps.length > 1 && matched.indexOf(false) != -1) {
+                    return false;
+                } else {
+                    //else true true (show in the store)
+                    return matched[0];
+                }
+            });
+
+        }
     },
     composeTink : function(list, idx, target, record, evt){
         from_user = Ext.getStore('profilestore').getPhoneNumber();
