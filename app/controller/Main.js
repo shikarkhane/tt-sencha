f1a0c300d517197b3dfa4d7212b94eaf59bb9af0
Nikhilm <shikarkhane@gmail.com>
2015-02-24 19:09:02 +0100

    on app resume

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -1,5 +1,6 @@
 Ext.define('ttapp.controller.Main', {
     extend: 'Ext.app.Controller',
+    xtype: 'main',
     requires: ['ttapp.util.Push'],
     config: {
         refs: {
@@ -9,6 +10,12 @@ Ext.define('ttapp.controller.Main', {
     },
     launch: function(){
         // when app comes to foreground
-        document.addEventListener("resume", ttapp.util.Push.takeUserPermissionForPushNotify, false);
+        document.addEventListener("resume", this.getApplication().getController('main').onResume, false);
+    },
+    onResume: function(){
+        // refresh push token
+        ttapp.util.Push.takeUserPermissionForPushNotify();
+        // refresh contacts list
+        ttapp.util.ContactsProxy.process(Ext.getStore('phonecontacts'));
     }
 });
