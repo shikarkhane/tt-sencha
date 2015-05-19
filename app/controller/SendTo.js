b2c8b0a2958443085b18b7bfb8df2a6dac5987de
Nikhilm <shikarkhane@gmail.com>
2015-05-19 21:01:45 +0200

    optimize contacts load

--- app/controller/SendTo.js
+++ app/controller/SendTo.js
@@ -107,55 +107,6 @@ Ext.define('ttapp.controller.SendTo', {
                 return false;
                });
         }
-/*
-            //the user could have entered spaces, so we must split them so we can loop through them all
-            var searches = value.split(' '),
-                regexps = [],
-                i;
-
-            //loop them all
-            for (i = 0; i < searches.length; i++) {
-                //if it is nothing, continue
-                if (!searches[i]) continue;
-
-                //if found, create a new regular expression which is case insenstive
-                regexps.push(new RegExp(searches[i], 'i'));
-            }
-
-            //now filter the store by passing a method
-            //the passed method will be called for each record in the store
-            store.filter(function(record) {
-                var matched = [];
-
-                //loop through each of the regular expressions
-                for (i = 0; i < regexps.length; i++) {
-                    var search = regexps[i],
-                        didMatch;
-                    if ( record.get('first_name') ){
-                        didMatch = record.get('first_name').match(search)
-                    }
-                    if ( record.get('last_name') && !didMatch ){
-                        didMatch = record.get('last_name').match(search)
-                    }
-                    if ( record.get('phone_number') && !didMatch){
-                        didMatch = record.get('phone_number').match(search)
-                    }
-
-                    //if it matched the first or last name, push it into the matches array
-                    matched.push(didMatch);
-                }
-
-                //if nothing was found, return false (dont so in the store)
-                if (regexps.length > 1 && matched.indexOf(false) != -1) {
-                    return false;
-                } else {
-                    //else true true (show in the store)
-                    return matched[0];
-                }
-            });
-
-        }
-*/
         
         if (field.getValue() == '') {
             Ext.getCmp('contactsListToChoose').setStore('');
