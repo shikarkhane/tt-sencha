0843da971fb2b5e933da905367a4bcfa7bc8472f
Nikhilm <shikarkhane@gmail.com>
2015-11-21 20:47:32 +0100

    user profile getting cleared

--- app/controller/Authenticate.js
+++ app/controller/Authenticate.js
@@ -8,7 +8,7 @@ Ext.define('ttapp.controller.Authenticate', {
         },
         control: {
             'authenticate': {
-                show: 'clearLocalStores'
+                show: 'setDialcode'
             },
             'confirmphonenumber': {
                 show: 'showPhoneNumber'
