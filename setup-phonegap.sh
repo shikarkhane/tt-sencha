d37a3af4c3076883d3bc595f35e6c8303765a136
Nikhilm <shikarkhane@gmail.com>
2015-11-23 13:55:35 +0100

    changed order and sms plugin(cause there was a warning)

--- setup-phonegap.sh
+++ setup-phonegap.sh
@@ -3,19 +3,20 @@ cordova create pg-tinktime "com.tinktime.app" "Tinktime"
 
 cd pg-tinktime
 
-cordova plugin add phonegap-plugin-push
-cordova plugin add cordova-plugin-camera
-cordova plugin add cordova-plugin-console
-cordova plugin add https://github.com/apache/cordova-plugin-contacts.git
-cordova plugin add cordova-plugin-device
-cordova plugin add cordova-plugin-file
-cordova plugin add cordova-plugin-file-transfer
-cordova plugin add com.rjfun.cordova.sms
-cordova plugin add cordova-plugin-filepath
-cordova plugin add cordova-plugin-google-analytics
-
 cordova platform add ios
 cordova prepare ios
 
 cordova platform add android
 cordova prepare android
+
+cordova plugin add phonegap-plugin-push
+cordova plugin add cordova-plugin-camera
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-contacts
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-file
+cordova plugin add cordova-plugin-file-transfer
+cordova plugin add cordova-plugin-sms
+cordova plugin add cordova-plugin-filepath
+cordova plugin add cordova-plugin-google-analytics
+
