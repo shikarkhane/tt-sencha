22b1b111341ac1202638c009b23ab2ed2cb5f4d2
Nikhil <nikhil.shikarkhane@comeon.com>
2015-12-03 15:04:32 +0100

    resume call looks same as load call

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -13,27 +13,27 @@ Ext.define('ttapp.controller.Main', {
         document.addEventListener("resume", this.onResume, false);
     },
     onResume: function(){
-        console.log('on resume');
+        console.log('on resume starts');
         ttapp.util.TrinketProxy.process(true, function() {
 
-       ttapp.util.Common.isUserVerifiedOnServer(function(success) {
-            // Initialize the main view
-            if (success) {
-                // refresh push token
-                ttapp.util.Push.takeUserPermissionForPushNotify();
+           ttapp.util.Common.isUserVerifiedOnServer(function(success) {
+                // Initialize the main view
+                if (success) {
+                    // refresh push token
+                    ttapp.util.Push.takeUserPermissionForPushNotify();
 
-                //update user image avatar
-                Ext.getStore('profilestore').setUserImage();
+                    //update user image avatar
+                    Ext.getStore('profilestore').setUserImage();
 
-                // refresh contacts list
-                ttapp.util.ContactsProxy.process(Ext.getStore('phonecontacts'));
-            }
-            else {
-                Ext.Viewport.add(Ext.create('ttapp.view.Authenticate'));
-            }
+                    // refresh contacts list
+                    ttapp.util.ContactsProxy.process(Ext.getStore('phonecontacts'));
+                }
+                else {
+                    Ext.Viewport.add(Ext.create('ttapp.view.Authenticate'));
+                }
+            });
+
         });
-
-        });
-        
+        console.log('on resume ends');
     }
 });
