f49ecf008bd763d92881a638d9d1cbaae57eb3cf
Nikhil <nikhil.shikarkhane@comeon.com>
2016-01-20 15:15:09 +0100

    show user selected instead of tinkgallery

--- app/controller/Trinket.js
+++ app/controller/Trinket.js
@@ -41,6 +41,12 @@ Ext.define('ttapp.controller.Trinket', {
         component.add(list);
         component.add(ttapp.util.Common.createMenuButton());
 
+        var profile_url = Ext.getStore('phonecontacts').getUserImage(window.contactSelected.data.phone_number);
+        if(!Ext.isEmpty(profile_url)) {
+            Ext.ComponentQuery.query('#trinketProfileImg')[0].setStyle({'background':'url('+profile_url+')'});
+            //Ext.getCmp('tinkchatimage').setStyle({'background':'url('+profile_url+')'});
+        }
 
+        Ext.select('.user-title').setHtml(getName(window.contactSelected.data.phone_number));
     }
 });
--- app/view/Trinket.js
+++ app/view/Trinket.js
@@ -12,28 +12,37 @@ Ext.define('ttapp.view.Trinket', {
                 cls: 'new-header',
                 items: [
                     {
-                        xtype: 'panel',
-                        cls: 'logo-green',
+                        xtype: 'button',
+                        cls: 'back-btn-icon trinket-back-btn',
                         docked: 'top',
-                        items:[
+                        listeners: [
                             {
-                                xtype: 'button',
-                                cls: 'back-btn-icon trinket-back-btn',
-                                docked: 'top',
-                                listeners: [
-                                    {
-                                        element: 'element',
-                                        event: 'tap',
-                                        fn: function() {
-                                            Ext.Viewport.animateActiveItem('phoneContacts', {type: 'slide', direction: 'right'});
-                                            ttapp.util.Analytics.trackView('Contacts');
-                                            return false;
-                                        }
-                                    }
-                                ]
+                                element: 'element',
+                                event: 'tap',
+                                fn: function() {
+                                    Ext.Viewport.animateActiveItem('phoneContacts', {type: 'slide', direction: 'right'});
+                                    ttapp.util.Analytics.trackView('Contacts');
+                                    return false;
+                                }
                             }
                         ]
+                    },
+                    {
+                        xtype: 'image',
+                        mode: 'image',
+                        cls:'header-user-img',
+                        setStyleHtmlContent: true,
+                        style: 'background:url(resources/images/user-icon.png)',
+                        itemId: 'trinketProfileImg',
+                        //src: 'resources/images/user-icon.png',
+                        docked: 'right',
                     }, {
+                        xtype: 'panel',
+                        html:'<div class="user-title"></div>',
+                        docked: 'top'
+                    }
+                    ,
+                    {
                         html:'Choose animation',
                         cls:'top-text-heading'
                     }
