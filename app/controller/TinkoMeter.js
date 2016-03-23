1594b2f10c9b9d73c3dbe99468180d8fd381abb4
Nikhilm <shikarkhane@gmail.com>
2016-03-23 22:08:57 +0100

    get social sharing urls from the server

--- app/controller/TinkoMeter.js
+++ app/controller/TinkoMeter.js
@@ -18,45 +18,78 @@ Ext.define('ttapp.controller.TinkoMeter', {
             }
         }
     },
+    shareNow: function(socialnetwork){
+        Ext.Viewport.mask({
+            xtype: 'loadmask',
+            html: '<img src="resources/images/green-loader.png" alt="loader">'
+        });
+
+        Ext.Ajax.request({
+            url: ttapp.config.Config.getBaseURL() + '/socialv2/' + socialnetwork + '/',
+            method: 'GET',
+            disableCaching: false,
+            success: function(response) {
+                var json = Ext.JSON.decode(response.responseText);
+                var sn = json.socialnetwork, url = json.url, imgurl = json.imgurl;
+
+                if ( sn == 'twitter'){
+                    window.plugins.socialsharing.available(function(isAvailable) {
+                        // the boolean is only false on iOS < 6
+                        if (isAvailable) {
+                            // now use any of the share() functions
+                            window.plugins.socialsharing.shareViaTwitter('Try Tinktime app. Time is a gift, share it!',
+                                null , url);
+                        }
+                    });
+                }
+                else if ( sn == 'facebook'){
+                    window.plugins.socialsharing.available(function(isAvailable) {
+                        // the boolean is only false on iOS < 6
+                        if (isAvailable) {
+                            // now use any of the share() functions
+                            window.plugins.socialsharing.shareViaFacebook('Try Tinktime app. Time is a gift, share it!',
+                                imgurl /* img */, url /* url */,
+                                function() {console.log('share ok')}, function(errormsg){console.log(errormsg)});
+                        }
+                    });
+                }
+                else if ( sn == 'instagram'){
+                    window.plugins.socialsharing.available(function(isAvailable) {
+                        // the boolean is only false on iOS < 6
+                        if (isAvailable) {
+                            // now use any of the share() functions
+                            window.plugins.socialsharing.canShareVia('instagram', 'msg', null, null, null,
+                                function(e){
+                                    shareViaInstagram('Try Tinktime app. Time is a gift, share it!',
+                                        imgurl, function() {console.log('share ok')}, function(errormsg){console.log(errormsg)})
+                                },
+                                function(e){console.log(e)});
+                        }
+                    });
+                }
+                else{
+                    console.log('no social network found');
+                }
+            },
+            failure: function(error) {
+                Ext.Viewport.setMasked(false);
+                Ext.Msg.alert('Error', 'Unable to fetch data.');
+            }
+        });
+    },
     tweetNow: function(callback, scope) {
         console.log('lets tweet');
 
-        window.plugins.socialsharing.available(function(isAvailable) {
-            // the boolean is only false on iOS < 6
-            if (isAvailable) {
-                // now use any of the share() functions
-                window.plugins.socialsharing.shareViaTwitter('Try Tinktime app. Time is a gift, share it!',
-                    null , ttapp.config.Config.getBaseURL() + '/social/twitter/');
-            }
-        });
+        this.shareNow('twitter');
     },
     fbNow: function(callback, scope) {
         console.log('lets fb');
-        window.plugins.socialsharing.available(function(isAvailable) {
-            // the boolean is only false on iOS < 6
-            if (isAvailable) {
-                // now use any of the share() functions
-                window.plugins.socialsharing.shareViaFacebook('Try Tinktime app. Time is a gift, share it!',
-                    null /* img */, ttapp.config.Config.getBaseURL() + '/social/facebook/' /* url */,
-                    function() {console.log('share ok')}, function(errormsg){console.log(errormsg)});
-            }
-        });
 
+        this.shareNow('facebook');
     },
     instaNow: function(callback, scope) {
         console.log('lets insta');
-        window.plugins.socialsharing.available(function(isAvailable) {
-            // the boolean is only false on iOS < 6
-            if (isAvailable) {
-                // now use any of the share() functions
-                window.plugins.socialsharing.canShareVia('instagram', 'msg', null, null, null,
-                    function(e){
-                        shareViaInstagram('Try Tinktime app. Time is a gift, share it!',
-                            ttapp.config.Config.getBaseURL() + '/social/instagram/', function() {console.log('share ok')}, function(errormsg){console.log(errormsg)})
-                    },
-                    function(e){console.log(e)});
-            }
-        });
 
+        this.shareNow('instagram');
     }
 });
